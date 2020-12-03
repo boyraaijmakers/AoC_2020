1d4848c05c30cc0e6d4ae22d0e72553bdbbead7a
Boy Raaijmakers <braaijmakers@deloitte.nl>
2020-12-03 07:58:09 +0100

    Day Three

--- src/main/java/org/boy/aoc/twentytwenty/exercises/Three.java
+++ src/main/java/org/boy/aoc/twentytwenty/exercises/Three.java
@@ -1,13 +1,32 @@
 package org.boy.aoc.twentytwenty.exercises;
 
+import java.util.Arrays;
+
 public class Three extends SolutionTemplate {
     @Override
     public String pointOne(String input) {
-        return super.pointOne(input);
+        return String.valueOf(solveWithSlope(input, 1, 3));
     }
 
     @Override
     public String pointTwo(String input) {
-        return super.pointTwo(input);
+        return String.valueOf(
+                solveWithSlope(input, 1, 1) *
+                solveWithSlope(input, 1, 3) *
+                solveWithSlope(input, 1, 5) *
+                solveWithSlope(input, 1, 7) *
+                solveWithSlope(input, 2, 1)
+        );
+    }
+
+    private long solveWithSlope(String input, int down, int right) {
+        long trees = 0;
+        String[] lines = input.split("\n");
+        int mapSize = lines[0].length();
+
+        for (int i = down; i < lines.length; i+=down) {
+            if (lines[i].charAt(((i / down) * right) % mapSize) == '#') trees++;
+        }
+        return trees;
     }
 }

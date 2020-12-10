d52e36bd6658bddfc55a4a6f9b5997fabe27bb09
Boy Raaijmakers <braaijmakers@deloitte.nl>
2020-12-10 07:52:17 +0100

    Day Ten

--- src/main/java/org/boy/aoc/twentytwenty/exercises/Ten.java
+++ src/main/java/org/boy/aoc/twentytwenty/exercises/Ten.java
@@ -1,13 +1,74 @@
 package org.boy.aoc.twentytwenty.exercises;
 
+import java.util.Arrays;
+
 public class Ten extends SolutionTemplate {
     @Override
     public String pointOne(String input) {
-        return super.pointOne(input);
+        int diffOne = 0;
+        int diffThree = 0;
+
+        int[] inputInts = Arrays.stream(input.split("\n"))
+                                    .mapToInt(Integer::parseInt)
+                                    .sorted()
+                                    .toArray();
+
+        int previousInt = 0;
+
+        for (int adapter: inputInts) {
+            switch (adapter - previousInt) {
+                case 1:
+                    diffOne++;
+                    break;
+                case 3:
+                    diffThree++;
+                    break;
+            }
+            previousInt = adapter;
+        }
+
+        return String.valueOf(diffOne * (diffThree+1));
     }
 
     @Override
     public String pointTwo(String input) {
-        return super.pointTwo(input);
+        int[] inputInts = Arrays.stream(input.split("\n"))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        int previousInt = 0;
+        long arrangements = 1;
+        int adapterWindow = 1;
+
+        for (int adapter: inputInts) {
+            if (adapter - previousInt == 3) {
+                arrangements *= countArrangements(adapterWindow);
+                adapterWindow = 0;
+            }
+
+            adapterWindow++;
+            previousInt = adapter;
+        }
+
+        arrangements *= countArrangements(adapterWindow);
+
+        return String.valueOf(arrangements);
+    }
+
+    private long countArrangements(int adapters) {
+        switch (adapters) {
+            case 1:
+            case 2:
+                return 1;
+            case 3:
+                return 2;
+            case 4:
+                return 4;
+            case 5:
+                return 7;
+
+        }
+        return -1;
     }
 }

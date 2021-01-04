be487a6fa3bb1171c5ed0716d8bb9e8eff048eb3
Boy Raaijmakers <braaijmakers@deloitte.nl>
2021-01-04 16:59:44 +0100

    Day Twentyfive

--- src/main/java/org/boy/aoc/twentytwenty/exercises/Twentyfive.java
+++ src/main/java/org/boy/aoc/twentytwenty/exercises/Twentyfive.java
@@ -3,7 +3,26 @@ package org.boy.aoc.twentytwenty.exercises;
 public class Twentyfive extends SolutionTemplate {
     @Override
     public String pointOne(String input) {
-        return super.pointOne(input);
+        String[] lines = input.split("\n");
+        int cardKey = Integer.parseInt(lines[0]), doorKey = Integer.parseInt(lines[1]);
+        int modulus = 20201227;
+
+        int loopSizeCard = 0, loopSizeDoor = 0;
+
+        long state = 1, multiplier = 7;
+
+        while(true) {
+               loopSizeCard++;
+               state = (state * multiplier) % modulus;
+               if (state == cardKey) break;
+        }
+
+        long encryptionKey = doorKey;
+
+        for (int i = 0; i < loopSizeCard - 1; i++) {
+            encryptionKey = (encryptionKey * doorKey) % modulus;
+        }
+        return String.valueOf(encryptionKey);
     }
 
     @Override
